c1a8ab92ff80f4a84c0b3d5fcf55639b4ecf2ce1
Suje <sujendranjanagiraman@gmail.com>
2024-12-08 12:35:43 +0530

    project commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <h2 className="text-light">Employee Agreement</h2>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
